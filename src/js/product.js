70cbd0fdab64d603d55aa24fddde755e840b93fe
Yuri Ziemba <yuri.fnunes@gmail.com>
2025-07-24 16:35:38 -0300

    create dynamic page details rendering

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,11 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import { getSearchParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetail from './ProductDetail.mjs';
 
-const CART_STORAGE_KEY = 'so-cart';
+const PRODUCT_PARAM = 'product';
 const dataSource = new ProductData('tents');
+const productId = getSearchParam(PRODUCT_PARAM);
 
-function addProductToCart(product) {
-  const productsInCart = getLocalStorage(CART_STORAGE_KEY) || [];
-  productsInCart.push(product);
-  setLocalStorage(CART_STORAGE_KEY, productsInCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productDetail = new ProductDetail(productId, dataSource);
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+productDetail.init();
